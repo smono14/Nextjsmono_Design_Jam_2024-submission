0e6c833082f7e0defd5b8453f22da90f0a368312
smono14 <154582148+smono14@users.noreply.github.com>
2025-02-08 05:09:29 +0500

    Add files via upload

---
+++ Atdoor/src/components/Home/Testimonials/testimonialsData.ts
@@ -0,0 +1,48 @@
+import { Testimonial } from "@/types/testimonial";
+
+const testimonialsData: Testimonial[] = [
+  {
+    review: `"I ordered a pair of shoes, and they arrived within 3 days! 
+    The quality is even better than expected. Definitely shopping here again!`,
+    authorName: "Syed Mohib",
+    authorImg: "/images/users/user-01.jpg",
+    authorRole: "Serial Entrepreneur",
+  },
+  {
+    review: `"Had an issue with my order, but customer support was super helpful.
+     They responded quickly. Highly recommend this store!`,
+    authorName: "Asifa Junaid",
+    authorImg: "/images/users/user-02.jpg",
+    authorRole: "Backend Developer",
+  },
+  {
+    review: `I love the amazing deals on this site! 
+    I got a branded smartwatch at a discounted price, and it's working perfectly!`,
+    authorName: "Aiman Khan",
+    authorImg: "/images/users/user-03.jpg",
+    authorRole: "Serial Entrepreneur",
+  },
+  {
+    review: `Bought a dress, and I was worried about the size,
+     but it fits perfectly! The size chart was accurate. Super happy with my purchase!`,
+    authorName: "Aun Rashid",
+    authorImg: "/images/users/user-01.jpg",
+    authorRole: "Entrepreneur",
+  },
+  {
+    review: `The product quality is top-notch, but it took a
+     bit longer to arrive than expected. Still, I'm happy with my purchase!`,
+    authorName: "Sadia Khan",
+    authorImg: "/images/users/user-02.jpg",
+    authorRole: "Serial Entrepreneur",
+  },
+  {
+    review: `This is my third time ordering from this website, 
+    and they never disappoint! Great products. Highly recommended!`,
+    authorName: "Muneeba Jawed",
+    authorImg: "/images/users/user-03.jpg",
+    authorRole: "Serial Entrepreneur",
+  },
+];
+
+export default testimonialsData;
